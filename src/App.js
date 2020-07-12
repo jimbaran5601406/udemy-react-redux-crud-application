26060417b2d6dcfa1e508692d7a113924d97bd0b
KeiFunatsuya <jimbaran5601406@gmail.com>
2020-07-12 17:29:00 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, {Component} from 'react';
+// jsxで記述する際にトランスコンパイルする関係でReactをimportする必要がある。
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
+    const dom = "Variable!";
     return (
-      <div>
-        <h1>Hello, world!</h1>
-      </div>
+      // returnで返すのは一つのブロックのみ
+      <React.Fragment>
+        <h1>Hello, {dom}!</h1>
+        <label htmlFor="bar">bar</label>
+        {/* 場所によって異なるコメントアウト */}
+        <input
+          type="text"
+          onClick={() => {
+            console.log("I am clicked.");
+          }}
+        />
+      </React.Fragment>
     );
   }
 }
