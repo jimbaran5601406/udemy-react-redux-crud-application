dfab436a3622cd63167804e64b9e5ba7a4fef490
KeiFunatsuya <jimbaran5601406@gmail.com>
2020-07-13 14:44:12 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 // jsxで記述する際にトランスコンパイルする関係でReactをimportする必要がある。
 import React, { Component } from "react";
 
-class App extends Component {
-  render() {
-    const dom = "Variable!";
-    return (
-      // returnで返すのは一つのブロックのみ
-      // ブロックをdivで囲ってしまうとWebページに意味のないdivタグが挿入れてしまうのでReact.Fragmentタグを使用する。
-      <React.Fragment>
-        <h1>Hello, {dom}!</h1>
-        <label htmlFor="bar">bar</label>
-        {/* 場所によって異なるコメントアウト */}
-        <input
-          type="text"
-          onClick={() => {
-            console.log("I am clicked.");
-          }}
-        />
-      </React.Fragment>
-    );
-  }
-}
+// Componentクラスを継承してAppクラスを定義しているのでクラスコンポーネントと呼ばれる。
+//class App extends Component {
+//render() {
+//const dom = "Variable!";
+//return (
+// returnで返すのは一つのブロックのみ
+// ブロックをdivで囲ってしまうとWebページに意味のないdivタグが挿入れてしまうのでReact.Fragmentタグを使用する。
+//<React.Fragment>
+//<h1>Hello, {dom}!</h1>
+//<label htmlFor="bar">bar</label>
+//{/* 場所によって異なるコメントアウト */}
+//<input
+//type="text"
+//onClick={() => {
+//console.log("I am clicked.");
+//}}
+//>
+//</React.Fragment>
+//);
+//}
+//}
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
