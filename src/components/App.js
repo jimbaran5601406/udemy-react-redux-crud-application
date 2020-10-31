0f736578ed778505713598dfb721c9962f959573
KeiFunatsuya <jimbaran5601406@gmail.com>
2020-10-31 09:14:46 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,27 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { increment, decrement } from "../actions";
 
 // 呼び出し元
-const App = () => <Counter></Counter>;
 // クラスコンポーネントの場合のみrender関数が必要
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    console.log(this.state);
-    this.state = { count: 0 };
-  }
-
-  handlePlusButton = () => {
-    // 直接状態を操作することは禁止
-    // this.state = { count: this.state.count};
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-
-  render() {
-    // setState();を実行したあとにrender();も実行される。
-    console.log("render");
-    return (
-      <React.Fragment>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
-      </React.Fragment>
-    );
-  }
+class App extends Component {
+	render() {
+		const props = this.props;
+		return (
+			<React.Fragment>
+				<div>count: {props.value}</div>
+				<button onClick={props.increment}>+1</button>
+				<button onClick={props.decrement}>-1</button>
+			</React.Fragment>
+		);
+	}
 }
+const mapStateToProps = (state) => ({ value: state.count.value });
+// const mapDispatchToProps = (dispatch) => ({
+// 	increment: () => dispatch(increment()),
+// 	decrement: () => dispatch(decrement())
+// });
+const mapDispatchToProps = { increment, decrement };
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
