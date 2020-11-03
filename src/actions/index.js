3e17693efe4d99b9cafe95e60ac08a60aea61cf1
KeiFunatsuya <jimbaran5601406@gmail.com>
2020-11-03 16:45:49 +0900

    implement delete feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 export const READ_EVENTS = "READ_EVENTS";
 export const CREATE_EVENT = "CREATE_EVENT";
+export const DELETE_EVENT = "DELETE_EVENT";
 
 const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1";
 const QUERYSTRING = "?token=token123";
@@ -20,5 +21,10 @@ export const postEvent = (values) => async (dispatch) => {
 	dispatch({ type: CREATE_EVENT, response });
 };
 
+// イベント削除
+export const deleteEvent = (id) => async (dispatch) => {
+	await axios.delete(`${ROOT_URL}/events${id}${QUERYSTRING}`);
+	dispatch({ type: DELETE_EVENT, id });
+};
 // typeはreducerがactionの識別子
 // 識別子であるtypeには定数を用いることが推奨される。
